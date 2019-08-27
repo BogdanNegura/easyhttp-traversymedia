b605f1a7b13a9ecbd805352abb022eb9c3e85054
Bogdan Negura <id.bogdann@yahoo.com>
2019-08-27 23:17:58 +0100

    make an PUT request and a DELETE request

--- app.js
+++ app.js
@@ -16,11 +16,30 @@ const data = {
 };
 
 // Create post
-http.post('https://jsonplaceholder.typicode.com/posts', 
-data, function(err, post) {
+// http.post('https://jsonplaceholder.typicode.com/posts', 
+// data, function(err, post) {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+// Update Post
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post) {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//             }
+// });
+
+// Delete post
+
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
     if(err) {
         console.log(err);
     } else {
-        console.log(post);
+        console.log(response);
     }
 });
--- easyhttp.js
+++ easyhttp.js
@@ -30,5 +30,31 @@ eastHTTP.prototype.post = function(url, data, callback) {
 }
 
 // Make an HTTP PUT Requests
+eastHTTP.prototype.put = function(url, data, callback) {
+    this.http.open('PUT', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+    let self = this;
+    this.http.onload = function() {
+        callback(null, self.http.responseText);
+        } 
+    
+    this.http.send(JSON.stringify(data));
+}
+
 
 // Make an HTTP DELETE Request
+
+eastHTTP.prototype.delete = function(url, callback) {
+    this.http.open('DELETE', url, true);
+
+    let self = this;
+    this.http.onload = function() {
+        if(self.http.status ===200) {
+            callback(null, 'Post Deleted');
+        } else {
+            callback('Error:' + self.http.status);
+        }
+    }
+    this.http.send();
+}
